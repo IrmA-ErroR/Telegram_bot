ceb27bb004c92db9e849476a6904979fa412c2a7
IrmA-ErroR <sveta.cat.kot@gmail.com>
2024-08-20 14:15:10 +0300

    Update

--- bot.py
+++ bot.py
@@ -1,13 +1,1 @@
-##### Эхо-бот
 import config
-import telebot
-
-bot = telebot.TeleBot(config.TOKEN) # Присваивание токена
-
-
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Функция повтора сообщений (название не важно)
-    bot.send_message(message.chat.id, message.text)
-
-
-bot.polling(none_stop=True) # Цикл
